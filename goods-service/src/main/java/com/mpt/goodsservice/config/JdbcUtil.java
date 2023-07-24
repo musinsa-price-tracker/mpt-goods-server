6808315261c570c6ec4b868575ab220848fef173
psg <seunggyu.dev@gmail.com>
2023-07-24 14:40:42 +0900

    setting application.yml

--- goods-service/src/main/java/com/mpt/goodsservice/config/JdbcUtil.java
+++ goods-service/src/main/java/com/mpt/goodsservice/config/JdbcUtil.java
@@ -1,6 +1,7 @@
 package com.mpt.goodsservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import java.sql.*;
 
