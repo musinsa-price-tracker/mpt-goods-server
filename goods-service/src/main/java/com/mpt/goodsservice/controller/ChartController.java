b8d51326db2284598d73b7456a8783ec10b40bb3
gxyxxng <gxyxxng@gmail.com>
2023-07-24 16:04:55 +0900

    feat goodslist goodsdetail chart

--- goods-service/src/main/java/com/mpt/goodsservice/controller/ChartController.java
+++ goods-service/src/main/java/com/mpt/goodsservice/controller/ChartController.java
@@ -3,12 +3,10 @@ package com.mpt.goodsservice.controller;
 import com.mpt.goodsservice.dao.ChartDao;
 import com.mpt.goodsservice.dao.GoodsDao;
 import com.mpt.goodsservice.domain.Chart;
-import com.mpt.goodsservice.domain.Goods;
 import com.mpt.goodsservice.dto.ChartDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -22,7 +20,7 @@ public class ChartController {
     @Autowired
     private GoodsDao goodsDao;
 
-    @GetMapping("/db/goods/{id}")
+    @GetMapping("/api/goods/{id}")//요상함
     public ResponseEntity<List<List<Long>>> getChartData(@PathVariable("id") int id) {
         List<Chart> chart = chartDao.getChartData(id);
         List<List<Long>> list = new LinkedList<>();
@@ -33,14 +31,5 @@ public class ChartController {
 
         return ResponseEntity.ok()
                 .body(list);
-    }
-
-    @GetMapping("/goods/{id}")
-    public String getGoodsDetail(@PathVariable("id") int id, Model model) {
-        Goods goods = goodsDao.getGoodsById(id);
-
-        model.addAttribute("goods", goods);
-
-        return "chart";
     }
 }
