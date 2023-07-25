3dab2ff5ddf07eabfb9f0d2fbcdd560235b02ed3
psg <seunggyu.dev@gmail.com>
2023-07-25 15:13:00 +0900

    goods-list sorting sql and pagination development

--- goods-service/src/main/java/com/mpt/goodsservice/config/JdbcUtil.java
+++ goods-service/src/main/java/com/mpt/goodsservice/config/JdbcUtil.java
@@ -1,37 +1,23 @@
 package com.mpt.goodsservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.sql.*;
 
 @Component
 public class JdbcUtil {
 
-    @Value("${spring.datasource.driver-class-name}")
-    private  String dbDriverName;
+    private final JdbcConfigValue jdbcConfigvalue;
 
-    @Value("${spring.datasource.url}")
-    private  String dbUrl;
-
-    @Value("${spring.datasource.username}")
-    private  String dbUsername;
-
-    @Value("${spring.datasource.password}")
-    private  String dbPassword;
-
-    @Value("${mpt.database-port}")
-    private  String dbPort;
-
-    @Value("${mpt.database-name}")
-    private  String dbName;
+    public JdbcUtil(JdbcConfigValue jdbcConfigValue) {
+        this.jdbcConfigvalue = jdbcConfigValue;
+    }
 
     public Connection getConnection() {
         Connection conn = null;
-        String jdbcUrl =
-                dbUrl + ":" + dbPort + "/" + dbName;
         try {
-            Class.forName(dbDriverName);
-            conn = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword);
+            Class.forName(jdbcConfigvalue.getDbDriverName());
+            conn = DriverManager.getConnection(jdbcConfigvalue.getJdbcUrl(), jdbcConfigvalue.getDbUsername(), jdbcConfigvalue.getDbPassword());
             if (conn != null) {
                 System.out.println("DB Connection Success");
             }
@@ -46,22 +32,28 @@ public class JdbcUtil {
         }
         return null;
     }
-    public static void close(PreparedStatement stmt, Connection conn) {
-        close(null,stmt,conn);
-    }
-    public static void close(ResultSet rs, PreparedStatement stmt, Connection conn)
-    {      try {if(rs != null)rs.close();
-    } catch (Exception e) {e.printStackTrace();
-    } finally { rs = null; }
-        try {if(stmt != null) stmt.close();
-        } catch (Exception e) {e.printStackTrace();
-        } finally {stmt = null; }
-        try {if(conn != null)conn.close();
-        } catch (Exception e) {e.printStackTrace();
-        } finally {conn = null; }
-    }
 
-//    public static void main(String[] args) {
-//        getConnection();
-//    }
+    public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+        try {
+            if (rs != null) rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rs = null;
+        }
+        try {
+            if (stmt != null) stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            stmt = null;
+        }
+        try {
+            if (conn != null) conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn = null;
+        }
+    }
 }
